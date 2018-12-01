914a1002acf66a77a91fee8cd330c13ce12b1952
Gary Krause <garyraykrause@outlook.com>
2018-12-01 16:38:20 -0500

    Advent of Code Day 1 Test 1
    Onw gold star.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) {
+                throw new NullReferenceException("Must use console argument for input.");
+            }
+            string[] inputLines = System.IO.File.ReadAllLines(args[0]);
+            int total = 0;
+            foreach (string line in inputLines) {
+                total += Convert.ToInt32(line);
+                System.Console.WriteLine("Adding: " + line);
+                System.Console.WriteLine("Total: " + total);
+            }
+            Console.WriteLine("Finished! Press Enter to continue.");
+            Console.ReadKey();
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # AdventOfCode2018
+
+My solutions for th 2018 Advent of Code. 
+Spoilers inside.
