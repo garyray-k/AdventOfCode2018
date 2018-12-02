a26659e388b3b393a04af65285370f614504d739
Gary Krause <garyraykrause@outlook.com>
2018-12-02 09:14:12 -0500

    Day1 Test2 Complete
    took way longer than I'd like to admit... I blame sleep deprevation.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdventOfCode
 {
@@ -10,12 +12,24 @@ namespace AdventOfCode
                 throw new NullReferenceException("Must use console argument for input.");
             }
             string[] inputLines = System.IO.File.ReadAllLines(args[0]);
-            int total = 0;
-            foreach (string line in inputLines) {
-                total += Convert.ToInt32(line);
-                System.Console.WriteLine("Adding: " + line);
-                System.Console.WriteLine("Total: " + total);
+            long total = 0;
+            List<long> totals = new List<long> {};
+            long answer = 0;
+            while (answer == 0) {
+                foreach (string line in inputLines) {
+                    total += Convert.ToInt64(line);
+                    if (totals.Contains(total)) {
+                        answer = total;
+                        Console.WriteLine("The answer is: " + total);
+                        break;
+                    }
+                    totals.Add(total);
+                    Console.WriteLine("Adding: " + line);
+                    Console.WriteLine("Total currently: " + total);
+                }
             }
+
+            Console.WriteLine("The total is: " + total);
             Console.WriteLine("Finished! Press Enter to continue.");
             Console.ReadKey();
 
