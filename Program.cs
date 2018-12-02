7486ff5fbdc2601562edb56e0c7f355a87875df5
Gary Krause <garyraykrause@outlook.com>
2018-12-02 15:44:44 -0500

    Day2 Done
    took some extra time to print it out pretty.

--- Program.cs
+++ Program.cs
@@ -9,6 +9,43 @@ namespace AdventOfCode
         static void Main(string[] args) {
             // Day1 Testing - FrequencyCalibration(args);
             string[] inputLines = ParseLinesFromInput(args);
+            // Day2Test1CharCounting(inputLines);
+            foreach (string line in inputLines) { 
+                char[] charsInLine = line.ToCharArray();
+                foreach (string comparisonLine in inputLines) {
+                    if (comparisonLine == line) {
+                        continue;
+                    } 
+                    char[] charsInComparison = comparisonLine.ToCharArray();
+                    if (charsInLine.Length != charsInComparison.Length) {
+                        continue;
+                    }
+                    int numberOfSameChars = 0;
+                    for (int x = 0; x < charsInLine.Length; x++) {
+                        if (charsInLine[x] == charsInComparison[x]) {
+                            numberOfSameChars++;
+                        }
+                    }
+                    if (numberOfSameChars == charsInLine.Length - 1) {
+                        Console.WriteLine(line);
+                        Console.WriteLine(comparisonLine);
+                        for (int x = 0; x < charsInLine.Length; x++) { 
+                            if (charsInLine[x] == charsInComparison[x]) {
+                                Console.Write(charsInLine[x]);
+                            }
+                        }
+                        Console.WriteLine("");
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine("Finished! Press Enter to continue.");
+
+            Console.ReadKey();
+        }
+
+        private static void Day2Test1CharCounting(string[] inputLines) {
             int twoCharCounter = 0;
             int threeCharCounter = 0;
             foreach (string line in inputLines) {
@@ -27,11 +64,9 @@ namespace AdventOfCode
             }
             Console.WriteLine("{0} instances of duplicates and {1} instances of triplicates.", twoCharCounter, threeCharCounter);
             Console.WriteLine("The answer is: " + (twoCharCounter * threeCharCounter));
-            Console.WriteLine("Finished! Press Enter to continue.");
-            Console.ReadKey();
         }
 
-        private static void FrequencyCalibration(string[] args) {
+        private static void FrequencyCalibrationDay1(string[] args) {
             string[] inputLines = ParseLinesFromInput(args);
             long total = 0;
             List<long> totals = new List<long> { };
